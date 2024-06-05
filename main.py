8a0e844ab5e1df3d9f1fd92a5aaf2fbcbe957632
kunlunrepo <482976531@qq.com>
2024-06-05 15:12:25 +0800

    使用uvicorn启动项目

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 # async def 定义异步函数的方法
 # @app.get("/") 定义路由
+# @app路由装饰器 get()请求方法 ""访问路径
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -13,3 +14,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+#运行服务器
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
